356a4e79db5f1414a1099de478716df1e3b187af
harry <459884392@qq.com>
2021-03-09 21:26:48 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -4,57 +4,81 @@ var xmlparser = require('express-xml-bodyparser');
 // let bodyParser = require('body-parser');
 import { js2xml, xml2js } from "xml-js";
 import { getSignature, decrypt, encrypt } from '@wecom/crypto';
-import { randomInt } from "crypto";
 let app = express();
 app.use(xmlparser());
 
 const token = "";
 const encodingAESKey = "";
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
 app.get('/wework/callback', (req, res) => {
     console.log(req.query);
-    var params:object = req.query;
+    var params: object = req.query;
     const signature = getSignature(token, params["timestamp"], params["nonce"], params["echostr"]);
 
-    res.send(signature);
+    if (signature != params["msg_signature"]) {
+        console.log("signature", signature);
+        console.warn("signature != msg_signature");
+        res.send("error");
+    }
+    else {
+        const { message } = decrypt(encodingAESKey, params["echostr"]);
+        console.log("message", message);
+        res.send(message);
+    }
 });
 
 app.post('/wework/callback', (req, res) => {
-    console.log(req.data);
-    const {message, id} = decrypt(encodingAESKey, req.data["Encrypt"]);
+    const params = req.body.xml;
+    console.log(params);
+    const { message, id } = decrypt(encodingAESKey, params["encrypt"][0]);
 
-    let obj: object = xml2js(message);
+    let obj: object = xml2js(message, {compact:true, ignoreComment: true, alwaysChildren: true});
 
-    console.log(obj);
+    console.log("get message", obj);
 
     let resp: any = {
-        "ToUserName": "",
-        "FromUserName": "fromUser",
-        "CreateTime": 1357290913,
-        "MsgType": "update_taskcard",
-        "TaskCard": {
-            "ReplaceName": "replace_name"
+        xml: {
+            "ToUserName": {"_cdata":"ToUserName"},
+            "FromUserName": {"_cdata":"FromUserName"},
+            "CreateTime": 1357290913,
+            "MsgType": {"_cdata": "update_taskcard"},
+            "TaskCard": {
+                "ReplaceName": {"_cdata": "replace_name"}
+            }
         }
     }
 
-    let xmlData = js2xml(resp);
+    console.log("real resp: ", resp);
+    let xmlData = js2xml(resp, {compact: true});
+    console.log("xmlData: ", xmlData);
 
     const ciphered = encrypt(encodingAESKey, xmlData, id);
 
     let timestamp = Date.now();
 
-    let nonce = randomInt(1000000);
+    let nonce = getRandomInt(100000000);
 
     const signature = getSignature(token, timestamp, nonce, ciphered);
 
     resp = {
-        TimeStamp: timestamp,
-        Nonce: nonce,
-        MsgSignature: signature,
-        Encrypt: ciphered
+        xml: {
+            TimeStamp: timestamp,
+            Nonce: nonce,
+            MsgSignature: {"_cdata": signature},
+            Encrypt: {"_cdata": ciphered}
+        }
     }
-    
-    res.send(resp);
+
+    let xmlResp = js2xml(resp, {compact: true});
+    console.log("xmlResp: ", xmlResp);
+
+    setTimeout(() => {
+        res.send(xmlResp);
+    }, 0);
 });
 
-app.listen(3000);
+app.listen(8080);
